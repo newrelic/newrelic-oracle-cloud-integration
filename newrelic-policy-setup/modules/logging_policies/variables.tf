440aa2bbff9cdee9f416a8062c036cc83205420a
Anusha Hegde <ahegde@newrelic.com>
2025-08-26 14:19:19 +0530

    logging policies for onboarding

---
+++ newrelic-policy-setup/modules/logging_policies/variables.tf
@@ -0,0 +1,43 @@
+variable "tenancy_ocid" {
+  type        = string
+  description = "OCI tenant OCID, more details can be found at https://docs.cloud.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#five"
+}
+
+variable "compartment_ocid" {
+  description = "The OCID of the compartment where resources will be created."
+  type        = string
+}
+
+variable "region" {
+  description = "The home region where the vault and policies will be created."
+  type        = string
+  default     = "us-ashburn-1"
+}
+
+variable "new_relic_tenancy_ocid" {
+  description = "The OCID of the New Relic tenancy to which access will be granted."
+  type        = string
+}
+
+variable "new_relic_group_ocid" {
+  description = "The OCID of the New Relic group that will be granted access."
+  type        = string
+}
+
+variable "newrelic_account_id" {
+  type        = string
+  sensitive   = true
+  description = "The New Relic account ID for sending metrics to New Relic endpoints"
+}
+
+variable "newrelic_ingest_api_key" {
+  type        = string
+  sensitive   = true
+  description = "The Ingest API key for sending logs to New Relic endpoints"
+}
+
+variable "newrelic_user_api_key" {
+  type        = string
+  sensitive   = true
+  description = "The User API key for Linking the OCI Account to the New Relic account"
+}
